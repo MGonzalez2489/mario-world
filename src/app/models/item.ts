4801954dda13fed0ce04d9f85e92eee165224117
Manuel <mgonzalez@pkglobal.com>
2020-09-18 12:26:15 -0600

    models

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -1,2 +1,6 @@
 export class Item {
+    id: number
+    image: string
+    title: string
+    description: string
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
+
+  items: Array<Item>= new Array<Item>
 
   constructor() { }
 }
