ee5ff7f907f2ad84f35dfd6a11dd53240d786381
Manuel Gonzalez <manuel_agh@hotmail.com>
2020-09-29 06:19:23 -0600

    reactive form

--- src/app/pages/items-form/items-form.component.ts
+++ src/app/pages/items-form/items-form.component.ts
@@ -9,30 +9,26 @@ import { Validators } from '@angular/forms';
 })
 export class ItemsFormComponent implements OnInit {
   itemForm = new FormGroup({
-    titleCtrl: new FormControl([Validators.required, Validators.minLength(3)]),
-    descriptionCtrl: new FormControl([
+    titleCtrl: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    descriptionCtrl: new FormControl('', [
       Validators.required,
       Validators.maxLength(50),
     ]),
   });
+  isSubmited: boolean;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  miSubmitFunction() {
+    this.isSubmited = true;
+    if (this.itemForm.valid) {
+      //llama al server y manda informacion
+      alert('formulario valido yahooooo!');
+    }
+  }
 }
-/* formulario
- >> Lo que debe de tener
-  1.- Submit function: es la funcion resultante del formulario (BP)
-  2.- Cancel function: es la funcion de cancelar la accion de uso del formulario (BP)
-  3.- Validaciones: el formulario es la parte donde se introduce o actualiza informacion
-      por lo tanto: debe de ser confiable cuando pase del formulario (BP)
-  4.- AYUDA E INFORMACION VISUAL: el usuario debe de ser informado porque su formulario es
-      valido/invalido y que parte del formulario es valido/invalido (CP)
-
-  >> Partes de un formulario
-
-  1.- Contenedor (cp)
-  2.- INPUTS: (REQUIRED)
-  3.- botones de accion (REQUIRED)
-
-*/
